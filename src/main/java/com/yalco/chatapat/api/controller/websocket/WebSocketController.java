d6ecd53199a366055ff9d05f20118f44a60ab0c0
Yalchan <yalcin_2009@abv.bg>
2021-08-15 17:56:39 +0300

    CHAT-22: enable secured websockets

---
+++ src/main/java/com/yalco/chatapat/api/controller/websocket/WebSocketController.java
@@ -0,0 +1,19 @@
+package com.yalco.chatapat.api.controller.websocket;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebSocketController {
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/message")
+    public void handleMessage() {
+
+    }
+}
