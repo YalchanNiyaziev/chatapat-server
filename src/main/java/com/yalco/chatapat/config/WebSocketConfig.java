61547bfb3f767449e32c4fa22667f13b59bc022c
Yalchan Niyaziev <yalcin_2009@abv.bg>
2021-08-27 17:44:50 +0300

    CHAT-27: six allow origins

--- src/main/java/com/yalco/chatapat/config/WebSocketConfig.java
+++ src/main/java/com/yalco/chatapat/config/WebSocketConfig.java
@@ -51,7 +51,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-connect")
-                .setAllowedOrigins("/*")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
